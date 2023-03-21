4e7e82fc4941f2429071293f4b083ed1a4b12d0d
Kuldipchopade <kuldipchopade775@gmail.com>
2023-03-21 19:33:26 +0530

    modern-website-initial commit

---
+++ src/components/Stats.jsx
@@ -0,0 +1,27 @@
+import styles from "../style";
+import { stats } from "../constants";
+const Stats = () => (
+  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+    {stats.map((stat) => (
+      <div
+        key={stat.id}
+        className={`flex-1 flex justify-start items-center flex-row m-3`}
+      >
+        <h4
+          className="font-poppins font-semibold text-[40px] xs:leading-[53px] leading-[43px] text-white
+        "
+        >
+          {stat.value}
+        </h4>
+        <p
+          className="font-poppins font-normal text-[20px] xs:leading-[26px] leading-[21px] text-white text-gradient uppercase ml-3
+        "
+        >
+          {stat.title}
+        </p>
+      </div>
+    ))}
+  </section>
+);
+
+export default Stats;
